a4a131bfce986ddf3b1799720a3e4145cbcfc328
Thorsten Kansy <tkansy@dotnetconsulting.eu>
2024-05-06 12:17:05 +0200

    Auto commit

---
+++ dotnetconsulting.Auth/dotnetconsulting.Auth/Program.cs
@@ -0,0 +1,62 @@
+// Disclaimer
+// Dieser Quellcode ist als Vorlage oder als Ideengeber gedacht. Er kann frei und ohne 
+// Auflagen oder Einschränkungen verwendet oder verändert werden.
+// Jedoch wird keine Garantie übernommen, dass eine Funktionsfähigkeit mit aktuellen und 
+// zukünftigen API-Versionen besteht. Der Autor übernimmt daher keine direkte oder indirekte 
+// Verantwortung, wenn dieser Code gar nicht oder nur fehlerhaft ausgeführt wird.
+// Für Anregungen und Fragen stehe ich jedoch gerne zur Verfügung.
+
+// Thorsten Kansy, www.dotnetconsulting.eu
+
+using dotnetconsulting.Auth.Code;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddAuthentication().AddBearerToken(IdentityConstants.BearerScheme);
+builder.Services.AddAuthorization();
+
+builder.Services.AddDbContext<AppIdentityDbContext>(o =>
+{
+    o.UseSqlite(@"DataSource=c:\temp\appidentity.db");
+});
+
+builder.Services.AddIdentityCore<AppUser>()
+    .AddEntityFrameworkStores<AppIdentityDbContext>()
+    .AddApiEndpoints();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.MapIdentityApi<AppUser>();
+
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", (ClaimsPrincipal appUser) =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            $"{appUser.Identity?.Name}: {summaries[Random.Shared.Next(summaries.Length)]}"
+        ))
+        .ToArray();
+
+    return forecast;
+}).RequireAuthorization();
+
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
